c8032566ae9ceed6efdf88cc2aa120731ba4ee01
gustavo <gustavo.a.lima21@aluno.senai.br>
2026-05-15 14:50:33 -0300

    feat: adiciona cors para integração com front

--- src/server.js
+++ src/server.js
@@ -7,10 +7,17 @@ import CuriosidadeRoute from './routes/CuriosidadeRoute.js';
 import EquipeRoute from './routes/EquipeRoute.js';
 import UsuarioRoute from './routes/UsuarioRoute.js';
 import ApiKey from './lib/middlewares/ApiKey.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(
+    cors({
+        origin: '*',
+        allowedHeaders: ['Content-Type', 'x-api-key'],
+    }),
+);
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -27,6 +34,7 @@ app.use('/usuarios',ApiKey, UsuarioRoute);
 app.use((req, res) => {
     res.status(404).json({ error: 'Rota não encontrada' });
 });
+
 
 app.listen(PORT, () => {
     console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
